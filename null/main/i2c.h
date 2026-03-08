8b938383b3f2839fcb46fc68eb7a45ea59236cbb
Jaden Velasco <jvelasco20081@gmail.com>
2026-03-07 19:08:03 -0800

    i2c init of ssd1306

--- null/main/i2c.h
+++ null/main/i2c.h
@@ -3,8 +3,13 @@
 #include "driver/i2c_master.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+#include "esp_lcd_panel_io.h" 
 #define 
 static const char * TAG = "I2C_OLED"
-struct i2c_master_bus_config_t init_OLED() 
-
+struct i2c_handles {
+	struct i2c_master_bus_handle_t bus_handler;
+	struct i2c_master_dev_handle_t dev_handler;
+}; 
+struct i2_master_dev_handle_t init_OLED();
+struct esp_lcd_panel_handel_t ssd1306_driver_init(struct i2c_master_dev_handle_t * dev_handle); 
 #endif 
--- null/main/null.c
+++ null/main/null.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 void app_main(void)
 {
+	
 
 }
